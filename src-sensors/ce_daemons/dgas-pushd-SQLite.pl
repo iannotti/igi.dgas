5015635483b2a5565e825cf8da8726486f1ac3a3
aguarise <aguarise>
2011-02-28 14:02:54 +0000

    New dgas-popualteJobTransSummary command which decouple the ordinary management of JTS to a separate command

--- src-sensors/ce_daemons/dgas-pushd-SQLite.pl
+++ src-sensors/ce_daemons/dgas-pushd-SQLite.pl
@@ -140,6 +140,7 @@ my $limit = ($configValues{maxThreadNumber})*3;
 #my $counter =0; 
 while( $keepGoing )
 {
+    LOGH->flush;
     my $t0 = [gettimeofday];
     my $successRecords = 0;
     my $failRecords = 0;       
@@ -199,6 +200,7 @@ while( $keepGoing )
 						$status[$threadNumber],
 						$key[$threadNumber]);
 			}
+    			LOGH->flush;
 			exit($status[$threadNumber]);
 		}
 		else
