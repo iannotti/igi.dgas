e020dcd6d7e08af8b7d183109d9fd51653149b66
aguarise <aguarise>
2011-05-26 14:13:21 +0000

    Changed log level

--- src-hlr-service/server/urForwardFactory.cpp
+++ src-hlr-service/server/urForwardFactory.cpp
@@ -194,8 +194,8 @@ int urForward::sendUsageRecords(hlrLocation &hlr, serverParameters& serverParms)
 		logBuff += ",lastTid=" + int2string(lastTid);
 		logBuff += ",totalNumberOfRecordsToSend=" + int2string(totalNumberOfRecordsToSend);
 		logBuff += ",totalNumberOfBurst=" + int2string(totalNumberOfBurst);
-		logBuff += ",sentBursts" + int2string(sentBursts);
-		hlr_log (logBuff,&logStream,5);
+		logBuff += ",sentBursts=" + int2string(sentBursts);
+		hlr_log (logBuff,&logStream,7);
 		logBuff = "Percentage of sent records:" + int2string(percentageSent);
 		logBuff += ",ETA:" + int2string(estimatedTimeOfArrival) + " secs";
 		hlr_log (logBuff,&logStream,5);
