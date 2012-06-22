6fbf781b196f265ecc2e0427a426fda38828fb3d
aguarise <aguarise>
2012-06-22 11:58:45 +0000

    Added simple authentication.

--- src-producers/producers/amqProducer.cpp
+++ src-producers/producers/amqProducer.cpp
@@ -1,7 +1,7 @@
 // DGAS (DataGrid Accounting System) 
 // Client APIs.
 // 
-// $Id: amqProducer.cpp,v 1.1.2.10 2012/06/22 11:55:14 aguarise Exp $
+// $Id: amqProducer.cpp,v 1.1.2.11 2012/06/22 11:58:45 aguarise Exp $
 // -------------------------------------------------------------------------
 // Copyright (c) 2001-2002, The DataGrid project, INFN, 
 // All rights reserved. See LICENSE file for details.
@@ -360,7 +360,7 @@ int dgasHlrRecordProducer (producerParms& parms)
 	map <string,string> confMap;
 	if ( dgas_conf_read ( parms.confFileName, &confMap ) != 0 )
 	{
-		cerr << "WARNING: Could not read conf file: " << confFileName << 
+		cerr << "WARNING: Could not read conf file: " << parms.confFileName <<
 endl;
 		cerr << "There can be problems processing the transaction" << endl;
 		if ( ( parms.amqBrokerUri == "" ) || ( parms.dgasAMQTopic == "" ) )
