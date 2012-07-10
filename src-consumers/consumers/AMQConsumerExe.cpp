87129862421ad0e3c63b867d24610996f11ebe39
aguarise <aguarise>
2012-07-10 11:53:25 +0000

    Consumer Tentative refactoring

--- src-consumers/consumers/AMQConsumerExe.cpp
+++ src-consumers/consumers/AMQConsumerExe.cpp
@@ -86,6 +86,16 @@ string outputDir = "";
 //string configFile = GLITE_DGAS_DEF_CONF;
 
 
+bool is_number(const std::string& s)
+{
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && std::isdigit(*it))
+		++it;
+	return !s.empty() && it == s.end();
+}
+
+
+
 int putLock(string lockFile)
 {
 	dgasLock Lock(lockFile);
