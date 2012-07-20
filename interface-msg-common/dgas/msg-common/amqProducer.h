8b8eb9aafc861c17e2e5963fb7dbf2ea5d825eea
aguarise <aguarise>
2012-07-20 12:02:37 +0000

    Refactor. Moved header to msg-common package

---
+++ interface-msg-common/dgas/msg-common/amqProducer.h
@@ -0,0 +1,76 @@
+// DGAS (DataGrid Accounting System) 
+// Client APIs.
+// 
+// $Id: amqProducer.h,v 1.1.2.1 2012/07/20 12:02:37 aguarise Exp $
+// -------------------------------------------------------------------------
+// Copyright (c) 2001-2002, The DataGrid project, INFN, 
+// All rights reserved. See LICENSE file for details.
+// -------------------------------------------------------------------------
+// Author: Andrea Guarise <andrea.guarise@to.infn.it>
+/***************************************************************************
+ * Code borrowed from:
+ *  authors   :
+ *  copyright : 
+ ***************************************************************************/
+//
+//    
+
+// ---------------------------------------------------------------------------
+//  Includes
+// ---------------------------------------------------------------------------
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include <memory>
+
+#define AMQ_PRODUCER_VERSION "4.0.x"
+
+class AmqProducer
+{
+
+private:
+	string outputMessage;
+
+public:
+
+	std::string amqBrokerUri;
+	std::string amqUsername;
+	std::string amqPassword;
+	std::string amqTopic;
+	bool useTopics;
+	bool clientAck;
+	bool persistentDelivery;
+	int verbosity;
+
+
+	AmqProducer(std::string amqBrokerUri,
+			std::string amqUsername, std::string amqPassword,
+			std::string amqTopic, bool useTopics = false, bool clientAck = false, bool persistentDelivery = false,
+			int verbosity = 0)
+	{
+		this->amqBrokerUri = amqBrokerUri;
+		this->amqUsername = amqUsername;
+		this->amqPassword = amqPassword;
+		this->amqTopic = amqTopic;
+		this->useTopics = useTopics;
+		this->clientAck = clientAck;
+		this->persistentDelivery = persistentDelivery;
+		this->verbosity = verbosity;
+	}
+
+	void setOutputMessage(string outputMessage);
+	int readConf(string& confFileName);
+	int run();
+};
+
+inline void AmqProducer::setOutputMessage(string outputMessage)
+{
+	this->outputMessage = outputMessage;
+}
+
+
+
+
