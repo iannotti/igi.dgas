ad1d17e79dcd0d1a2de6e7fd6429f7ce7275dd87
aguarise <aguarise>
2012-07-27 13:17:03 +0000

    added collectorPollInterval

--- src-sensors/ce_daemons/dgas-urcollector-SQLite.pl
+++ src-sensors/ce_daemons/dgas-urcollector-SQLite.pl
@@ -108,6 +108,7 @@ my %configValues   = (
 	collectorLogFileName    => "/var/log/dgas/dgas_urcollector.log",
 	collectorBufferFileName => "/var/spool/dgas/dgasCollectorBuffer",
 	mainPollInterval        => "5",
+	collectorPollInterval   => "5",
 	timeInterval            => "1",
 	jobPerTimeInterval      => "500",
 	ignoreJobsLoggedBefore  => $DEF_IGNORE_JOBS_LOGGED_BEFORE,
@@ -225,6 +226,7 @@ my $glueLdifFile            = $configValues{glueLdifFile};
 my $collectorLockFileName   = $configValues{collectorLockFileName};
 my $collectorBufferFileName = $configValues{collectorBufferFileName};
 my $mainPollInterval        = $configValues{mainPollInterval};
+my $collectorPollInterval   = $configValues{collectorPollInterval};
 my $timeInterval            = $configValues{timeInterval};
 my $jobPerTimeInterval      = $configValues{jobPerTimeInterval};
 my $maxNumRecords           = $configValues{maxNumRecords};
@@ -570,6 +572,13 @@ MAIN: while ($keepGoing)
 # print "".localtime().": Waiting for new jobs to finish. Sleeping for $mainPollInterval seconds.";
 		my $secsWaited = 0;
 		while ( $keepGoing && $secsWaited < $mainPollInterval )
+		{
+			usleep(100000);
+			$secsWaited++;
+		}
+		#additional sleep for fine tuning of resource consumption use collectorPollInterval to set this (this is far to be optimal FIXME)
+	    $secsWaited = 0;
+		while ( $keepGoing && $secsWaited < $collectorPollInterval )
 		{
 			usleep(100000);
 			$secsWaited++;
@@ -2175,6 +2184,10 @@ sub parseConf
 		if (/^mainPollInterval\s*=\s*\"(.*)\"$/)
 		{
 			$configValues{mainPollInterval} = $1;
+		}
+		if (/^collectorPollInterval\s*=\s*\"(.*)\"$/)
+		{
+			$configValues{collectorPollInterval} = $1;
 		}
 		if (/^timeInterval\s*=\s*\"(.*)\"$/)
 		{
