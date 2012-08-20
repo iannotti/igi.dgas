d2d1f5433ad4f82382e766d4292138fe7f2a6fe4
aguarise <aguarise>
2012-08-20 10:09:13 +0000

    Port from 4.0.0 to 4.2.0

---
+++ interface-consumers/glite/dgas/dgas-consumers/consumers/AMQConsumer.h
@@ -0,0 +1,57 @@
+// DGAS (DataGrid Accounting System) 
+// Client APIs.
+// 
+// $Id: AMQConsumer.h,v 1.1.4.2 2012/08/20 10:09:13 aguarise Exp $
+// -------------------------------------------------------------------------
+// Copyright (c) 2001-2002, The DataGrid project, INFN, 
+// All rights reserved. See LICENSE file for details.
+// -------------------------------------------------------------------------
+// Author: Andrea Guarise <andrea.guarise@to.infn.it>
+ /***************************************************************************
+ * Code borrowed from:
+ *  authors   :
+ *  copyright : 
+ ***************************************************************************/
+//
+//    
+
+// ---------------------------------------------------------------------------
+//  Includes
+// ---------------------------------------------------------------------------
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <vector>
+
+#define AMQ_CONSUMER_VERSION "protoA"
+
+using namespace std;
+
+class consumerParms {
+	public:
+		string confFileName;
+		string amqBrokerUri;
+		string amqUsername;
+		string amqPassword;
+		string amqClientId;
+		string lockFileName;
+		string logFileName; 
+		string dgasAMQTopic;
+		string useTopics;
+		string clientAck;
+		string hlrSqlTmpDBName;
+		string hlrSqlDBName;
+		string hlrSqlServer;
+		string hlrSqlUser;
+		string hlrSqlPassword;
+		string name;
+		string selector;
+		string noLocal;
+		string durable;
+};
+
+int AMQConsumer(consumerParms& parms);
+
+
